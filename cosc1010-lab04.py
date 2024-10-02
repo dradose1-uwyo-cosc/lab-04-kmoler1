e7ebec3e3e60a058d9541f1b5169cefbd491442f
kmoler1 <kmoler1@uwyo.edu>
2024-10-02 21:15:24 +0000

    while loop

--- cosc1010-lab04.py
+++ cosc1010-lab04.py
@@ -99,15 +99,15 @@ min_temps = [
 # Output the results of both calculations.
 min_mean=min_temps[0]
 max_mean=max_temps[0]
-#i=1
-#while(i<len(min_temps)):
-#    min_mean+=min_temps[i]
-#    max_mean+=max_temps[i]
-#    i=i+1
-for temp in min_temps:
-    min_mean+=temp
-for temp in max_temps:
-    max_mean+=temp
+i=1
+while(i<len(min_temps)):
+    min_mean+=min_temps[i]
+    max_mean+=max_temps[i]
+    i=i+1
+#for temp in min_temps:
+#    min_mean+=temp
+#for temp in max_temps:
+#    max_mean+=temp
 min_mean=int(min_mean/len(min_temps))
 max_mean=int(max_mean/len(max_temps))
 print(f"The min average is {min_mean} and the max average is {max_mean}")
